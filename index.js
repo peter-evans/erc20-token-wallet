62f2666239505852972decf523bc0d822505ca4a
peter-evans <peter-evans@users.noreply.github.com>
2018-04-11 14:40:15 +0900

    Fix for undefined wallet address

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function displayProviderInfo() {
 function startApp() {
     var account = 'undefined';
     var accountInterval = setInterval(function() {
-        if (web3.eth.accounts[0] == 'undefined') {
+        if (typeof web3.eth.accounts[0] == 'undefined') {
             document.getElementById("address").innerHTML = '';
             document.getElementById("ethBalance").innerHTML = '';
             document.getElementById("tokenBalance").innerHTML = '';
